e22b6e8d3b47d03537f77b3afa49b7a7fafc55ec
jananee009 <jananee009@gmail.com>
2016-07-21 16:48:25 -0400

    Code Refactoring
    Removed Class structure from Common.py. Made changes to file to reflect
    this removal.

--- Euler_Problem33.py
+++ Euler_Problem33.py
@@ -8,7 +8,7 @@ import math
 import time
 import Common
 
-common = Common.Common()
+
 
 
 		
@@ -18,13 +18,13 @@ def findDigitCancellingFractions():
 	for numerator in range(10,100): # since we are interested only in fractions having 2 digits in numerator and denominator.
 		for denominator in range(10,100):
 			# if value of fraction > 1 or if the units place of numerator or denominator has a 0, skip and go to next number.
-			if (   (numerator/denominator >= 1) or (common.getDigits(numerator)[1] == 0) or (common.getDigits(denominator)[1] == 0)):
+			if (   (numerator/denominator >= 1) or (Common.getDigits(numerator)[1] == 0) or (Common.getDigits(denominator)[1] == 0)):
 				pass
 			else:
-				if (common.getDigits(numerator)[1] == common.getDigits(denominator)[0]):	# Units digit of numerator equals the tens digit of denominator
+				if (Common.getDigits(numerator)[1] == Common.getDigits(denominator)[0]):	# Units digit of numerator equals the tens digit of denominator
 
 					# After digit cancelling, if value of incorrect fraction equals value of correct fraction, we have a winner.  
-					if (float(common.getDigits(numerator)[0]) / float(common.getDigits(denominator)[1]) == float(numerator)/float(denominator)):
+					if (float(Common.getDigits(numerator)[0]) / float(Common.getDigits(denominator)[1]) == float(numerator)/float(denominator)):
 						digit_cancelling_fractions.append((numerator,denominator))
 
 
@@ -36,15 +36,15 @@ def findDigitCancellingFractions():
 		product_numerator = product_numerator*fract[0] 
 		product_denominator = product_denominator*fract[1]
 	
-	# reduce the fraction to its lowest common term
-	reducedform = common.reduceFractionToLowestCommonTerms((product_numerator,product_denominator)) 			
+	# reduce the fraction to its lowest Common term
+	reducedform = Common.reduceFractionToLowestCommonTerms((product_numerator,product_denominator)) 			
 	return	reducedform		
 
 
 	
 def main():
 	start_time = time.time()
-	print "The value of the denominator is: ", findDigitCancellingFractions()
+	print "The value of the denominator is: ", findDigitCancellingFractions()[1]
 	print "Problem solved in %s seconds " % (time.time()-start_time)
 	
 		
